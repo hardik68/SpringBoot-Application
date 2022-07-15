247cdf6dcab6b3d80f8c93af62391161562e3989
Hardik.Setia <setiahardik68@gmail.com>
2022-07-15 11:45:45 +0530

    validate otp

--- src/main/java/com/esample/springrest/service/OtpServiceImpl.java
+++ src/main/java/com/esample/springrest/service/OtpServiceImpl.java
@@ -3,6 +3,8 @@ package com.esample.springrest.service;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,39 +18,45 @@ import com.esample.springrest.repository.UserOtpRepository;
 public class OtpServiceImpl implements OtpService {
 	@Autowired(required = true)
 	private UserOtpRepository otpRepository;
-	
+
 	@Value("${otp.expiry.time}")
 	private int expiryTime;
-	private  String getRandomNumberint() {
-	    // It will generate 4 digit random Number.
-	    // from 1000 to 9999
-	    Random rnd = new Random();
-     int number = rnd.nextInt(9999);
-	    //int number = 1;
+
+	private String getRandomNumberint() {
+		// It will generate 4 digit random Number.
+		// from 1000 to 9999
+		Random rnd = new Random();
+		int number = rnd.nextInt(9999);
+		//int number = 1;
 
 
-	    // this will convert any number sequence into 4 character.
-	    return String.format("%04d", number);
+		// this will convert any number sequence into 4 character.
+		return String.format("%04d", number);
 	}
 
-	
+
 	public String genrateOtp(MobileNumberInput mobileNumberInput) {
-		String otp=getRandomNumberint();
-		UserOtp userOtp=new UserOtp();
-		userOtp.setMobileNumber(mobileNumberInput.getMobileNumber());
-	      java.sql.Date date=new java.sql.Date(System.currentTimeMillis());
-	      java.sql.Date expiryDate=new java.sql.Date(System.currentTimeMillis()+expiryTime);
+		String otp = getRandomNumberint();
+		UserOtp userOtp = new UserOtp();
+		if(isValidMobileNo(mobileNumberInput.getMobileNumber())){
+			userOtp.setMobileNumber(mobileNumberInput.getMobileNumber());
+			java.sql.Date date = new java.sql.Date(System.currentTimeMillis());
+			java.sql.Date expiryDate = new java.sql.Date(System.currentTimeMillis() + expiryTime);
 
-		userOtp.setExpiryTime(expiryDate);
-		userOtp.setGenratedAt(date);
-		userOtp.setOtp(otp);
-		
-	    otpRepository.save(userOtp);
-		Map<String, String> otpStorageMap=new HashMap<>();
-		otpStorageMap.put(mobileNumberInput.getMobileNumber(), otp);
+			userOtp.setExpiryTime(expiryDate);
+			userOtp.setGenratedAt(date);
+			userOtp.setOtp(otp);
+
+			otpRepository.save(userOtp);
+			Map<String, String> otpStorageMap = new HashMap<>();
+			otpStorageMap.put(mobileNumberInput.getMobileNumber(), otp);
 			return otp;
+		}
+		else{
+			return "Not a valid Mobile Number";
+		}
 	}
-	
+
 	public boolean validateOtp(ValidateOtpRequest validateOtpRequest) {
 
 		//fetching generated otp using phone no
@@ -57,7 +65,11 @@ public class OtpServiceImpl implements OtpService {
 
 		if (userOtp != null)
 			System.out.println("### Response ###" + userOtp.getOtp() + " " + userOtp.getMobileNumber());
-		//compare  request otp and already generated otp 
+		if(null != userOtp && userOtp.getOtp().equals(validateOtpRequest.getOtp()))
+			return true;
+		else
+			return false;
+		//compare  request otp and already generated otp
 		//logic time validation , otp validation
 		//return true;
 		// }else {
@@ -65,8 +77,24 @@ public class OtpServiceImpl implements OtpService {
 		// }
 		//RESPONSE
 		//SAME sUCCES VALIDATE IF  FAILED
-		return true;
-// if(userOtp.getOtp().equalsIgnoreCase(validateOtpRequest.getOtp()) && (userOtp.getExpiryTime()+300000 >System.currentTimeMillis())) {
+
+		//return true;
+//
 	}
+
+	public static boolean isValidMobileNo(String str) {
+//(0/91): number starts with (0/91)
+//[7-9]: starting of the number may contain a digit between 0 to 9
+//[0-9]: then contains digits 0 to 9
+		Pattern ptrn = Pattern.compile(
+		"^[0-9]{10}$"
+
+);
+//the matcher() method creates a matcher that will match the given input against this pattern
+		Matcher match = ptrn.matcher(str);
+//returns a boolean value
+		return (match.find() && match.group().equals(str));
+	}
+
 
 }
--- src/main/java/com/esample/springrest/service/RetailerService.java
+++ src/main/java/com/esample/springrest/service/RetailerService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class RetailerService {
@@ -18,13 +20,13 @@ public class RetailerService {
 
     public RetailerResponse retailerOnboard(RetailerInput retailerInput) {
         try {
-            if (retailerInput.getAdharcardNo() == null || retailerInput.getAdharcardNo().equals("")) {
+            if (!isValidAadhaarNumber(retailerInput.getAdharcardNo())) {
                 return new RetailerResponse("Failure", "Aadhar Number not valid");
             }
             if (retailerInput.getMobile_number() == null || retailerInput.getMobile_number().equals("")) {
                 return new RetailerResponse("Failure", "Mobile Number not valid");
             }
-            if (retailerInput.getPanNo() == null || retailerInput.getPanNo().equals("")) {
+            if (!isValidPanCardNo(retailerInput.getPanNo())) {
                 return new RetailerResponse("Failure", "Pan Number not valid");
             }
             if (retailerInput.getEmail() == null || retailerInput.getEmail().equals("")) {
@@ -42,12 +44,59 @@ public class RetailerService {
             retailerUser.setLast_name(retailerInput.getLast_name());
             RetailerUser userReturned = retailerRepository.save(retailerUser);
             return new RetailerResponse("SUCCESS", "Successfully signed up.");
-        }catch (Exception ex) {
-            return new RetailerResponse("FAILURE",ex.getMessage());
+        } catch (Exception ex) {
+            return new RetailerResponse("FAILURE", ex.getMessage());
+        }
+    }
+        public static boolean isValidPanCardNo(String panCardNo)
+        {
+
+            // Regex to check valid PAN Card number.
+            String regex = "[A-Z]{5}[0-9]{4}[A-Z]{1}";
+
+            // Compile the ReGex
+            Pattern p = Pattern.compile(regex);
+
+            // If the PAN Card number
+            // is empty return false
+            if (panCardNo == null)
+            {
+                return false;
+            }
+
+            // Pattern class contains matcher() method
+            // to find matching between given
+            // PAN Card number using regular expression.
+            Matcher m = p.matcher(panCardNo);
+
+            // Return if the PAN Card number
+            // matched the ReGex
+            return m.matches();
+        }
+        public static boolean isValidAadhaarNumber(String aadhar)
+        {
+            // Regex to check valid Aadhaar number.
+            String regex = "^[2-9]{1}[0-9]{3}\\s[0-9]{4}\\s[0-9]{4}$";
+
+            // Compile the ReGex
+            Pattern p = Pattern.compile(regex);
+
+            // If the string is empty
+            // return false
+            if (aadhar == null) {
+                return false;
+            }
+
+            // Pattern class contains matcher() method
+            // to find matching between given string
+            // and regular expression.
+            Matcher m = p.matcher(aadhar);
+
+            // Return if the string
+            // matched the ReGex
+            return m.matches();
         }
 
-
-        }
 
 
     }
